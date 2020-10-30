33e3c4481e2f02e2db95945b52a4c10400b77257
Erick Daniel Padilla <erickdelpavo@gmail.com>
2020-10-29 17:25:52 -0700

    Added Taxcon to main page

--- global.js
+++ global.js
@@ -1,8 +1,10 @@
 console.log('Global.js')
 
 const AppStoreLink_PW = "https://apps.apple.com/us/app/phone-widget/id1537333783"
+const AppStoreLink_TC = "https://apps.apple.com/us/app/taxcon/id1317537747"
 
 // Change links on runtime
 window.onload = function(){
   document.getElementById("appStoreBadge_PW").href = AppStoreLink_PW
+  document.getElementById("appStoreBadge_TC").href = AppStoreLink_TC
 };
