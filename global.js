d2a0d87439c4ab9de22194fa29b8fe1749f01d8c
Erick Daniel Padilla <erickdelpavo@gmail.com>
2020-11-01 23:09:17 -0700

    Test fix

--- global.js
+++ global.js
@@ -5,8 +5,8 @@ const AppStoreLink_TC = "https://apps.apple.com/us/app/taxcon/id1317537747"
 
 // Change links on runtime
 window.onload = function(){
-  document.getElementById("appStoreBadge_PW").href = AppStoreLink_PW
-  document.getElementById("appStoreBadge_PW2").href = AppStoreLink_PW
-  document.getElementById("appStoreBadge_TC").href = AppStoreLink_TC
-  document.getElementById("appStoreBadge_TC2").href = AppStoreLink_TC
+  document.getElementById("appStoreBadge_PW")?.href = AppStoreLink_PW
+  document.getElementById("appStoreBadge_PW2")?.href = AppStoreLink_PW
+  document.getElementById("appStoreBadge_TC")?.href = AppStoreLink_TC
+  document.getElementById("appStoreBadge_TC2")?.href = AppStoreLink_TC
 };
